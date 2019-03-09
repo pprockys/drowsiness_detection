ffc34013be8ef7ab5b1cb300d64af1c58ecbb28f
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2019-03-09 23:19:44 +0530

    Update detect_drowsiness.py
    credit goes to _https://www.pyimagesearch.com/2017/05/08/drowsiness-detection-opencv/

--- detect_drowsiness.py
+++ detect_drowsiness.py
@@ -1,4 +1,4 @@
-#source_https://www.pyimagesearch.com/2017/05/08/drowsiness-detection-opencv/
+#source_ https://www.pyimagesearch.com/2017/05/08/drowsiness-detection-opencv/
 # USAGE
 # python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
 # python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
